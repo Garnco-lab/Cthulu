7e32f5fdaa8877c414e570d7371f2c9f53c90d87
Steven <stevendeleonwork@gmaill.com>
2022-01-29 22:34:48 -0800

    updated using different tutorial, found out about global spaces

--- __main__.py
+++ __main__.py
@@ -1,72 +1,103 @@
-import pygame
-import sys
-
-
-# initializing the constructor
+#!/usr/bin/python3.4
+# Setup Python ----------------------------------------------- #
+import pygame, sys
+ 
+# Setup pygame/window ---------------------------------------- #
+mainClock = pygame.time.Clock()
+from pygame.locals import *
 pygame.init()
+pygame.display.set_caption('game base')
+screen = pygame.display.set_mode((500, 500),0,32)
+ 
+font = pygame.font.SysFont(None, 20)
+ 
+def draw_text(text, font, color, surface, x, y):
+	textobj = font.render(text, 1, color)
+	textrect = textobj.get_rect()
+	textrect.topleft = (x, y)
+	surface.blit(textobj, textrect)
+ 
+click = False
+ 
+global sanitynum 
 
-# screen resolution
-res = (720,720)
+sanitynum = 18
 
-# opens up a window
-screen = pygame.display.set_mode(res)
+def main_menu():
+	global sanitynum
 
-# white color
-color = (255,255,255)
+	while True:
+ 
+		screen.fill((0,0,0))
+		draw_text('main menu', font, (255, 255, 255), screen, 20, 20)
+		mx, my = pygame.mouse.get_pos()
+ 
+		button_1 = pygame.Rect(50, 100, 200, 50)
+		button_2 = pygame.Rect(50, 200, 200, 50)
+		button_3 = pygame.Rect(50, 300, 200, 50 )
+		if button_1.collidepoint((mx, my)):
+			if click:
+				game()
+		if button_2.collidepoint((mx, my)):
+			if click:
+				options()
+		if button_3.collidepoint((mx, my)):
+			if click:
+				sanitynum += 1
+				print(sanitynum)
 
-# light shade of the button
-color_light = (170,170,170)
-
-# dark shade of the button
-color_dark = (100,100,100)
-
-# stores the width of the
-# screen into a variable
-width = screen.get_width()
-
-# stores the height of the
-# screen into a variable
-height = screen.get_height()
-
-# defining a font
-smallfont = pygame.font.SysFont('Corbel',35)
-
-# rendering a text written in
-# this font
-text = smallfont.render('quit' , True , color)
-
-while True:
-	
-	for ev in pygame.event.get():
-		
-		if ev.type == pygame.QUIT:
-			pygame.quit()
-			
-		#checks if a mouse is clicked
-		if ev.type == pygame.MOUSEBUTTONDOWN:
-			
-			#if the mouse is clicked on the
-			# button the game is terminated
-			if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
+		pygame.draw.rect(screen, (255, 0, 0), button_1)
+		pygame.draw.rect(screen, (255, 0, 0), button_2)
+		pygame.draw.rect(screen, (255, 0, 0), button_3)
+ 
+		click = False
+		for event in pygame.event.get():
+			if event.type == QUIT:
 				pygame.quit()
 				sys.exit()
-	# fills the screen with a color
-	screen.fill((60,25,60))
-	
-	# stores the (x,y) coordinates into
-	# the variable as a tuple
-	mouse = pygame.mouse.get_pos()
-	
-	# if mouse is hovered on a button it
-	# changes to lighter shade
-	if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
-		pygame.draw.rect(screen,color_light,[width/2,height/2,140,40])
+			if event.type == KEYDOWN:
+				if event.key == K_ESCAPE:
+					pygame.quit()
+					sys.exit()
+			if event.type == MOUSEBUTTONDOWN:
+				if event.button == 1:
+					click = True
+ 
+		pygame.display.update()
+		mainClock.tick(60)
+ 
+def game():
+	running = True
+	while running:
+		screen.fill((0,0,0))
 		
-	else:
-		pygame.draw.rect(screen,color_dark,[width/2,height/2,140,40])
-	
-	# superimposing the text onto our button
-	screen.blit(text , (width/2+50,height/2))
-	
-	# updates the frames of the game
-	pygame.display.update()
+		draw_text('game', font, (255, 255, 255), screen, 20, 20)
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+			if event.type == KEYDOWN:
+				if event.key == K_ESCAPE:
+					running = False
+		
+		pygame.display.update()
+		mainClock.tick(60)
+ 
+def options():
+	running = True
+	while running:
+		screen.fill((0,0,0))
+ 
+		draw_text('options', font, (255, 255, 255), screen, 20, 20)
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+			if event.type == KEYDOWN:
+				if event.key == K_ESCAPE:
+					running = False
+		
+		pygame.display.update()
+		mainClock.tick(60)
+ 
+main_menu()
